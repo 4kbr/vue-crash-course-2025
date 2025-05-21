19ca01fb954eb7d008b9eedcb588b4cf5beedc29
4kbr <>
2025-05-21 13:39:36 +0700

    init vue router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Becoming a Powerfull Vue Dev " sub-title="And find the Vue job that fits your skills and needs" />
-  <HomeCards />
-  <JobListings :limit="3" :show-button="true" />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+  <Hero title="Becoming a Powerfull Vue Dev " sub-title="And find the Vue job that fits your skills and needs" />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true" />
+</template>
